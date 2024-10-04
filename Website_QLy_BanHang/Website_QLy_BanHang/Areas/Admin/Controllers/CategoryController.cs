ec871ae33471c8d998f7d2c224277b77d0f6b042
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2024-10-04 16:56:47 +0700

    Project_Day3
    Categories - Create - Status

--- Website_QLy_BanHang/Website_QLy_BanHang/Areas/Admin/Controllers/CategoryController.cs
+++ Website_QLy_BanHang/Website_QLy_BanHang/Areas/Admin/Controllers/CategoryController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyClass.DAO;
 using MyClass.Model;
+using Website_QLy_BanHang.Library;
 
 namespace Website_QLy_BanHang.Areas.Admin.Controllers
 {
@@ -42,6 +43,8 @@ namespace Website_QLy_BanHang.Areas.Admin.Controllers
         // GET: Admin/Category/Create
         public ActionResult Create()
         {
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"),"Id","Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
             return View();
         }
 
@@ -52,7 +55,29 @@ namespace Website_QLy_BanHang.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-               categoriesDAO.Insert(categories);
+                //Xu Ly tu dong CreateAt
+                categories.CreateAt = DateTime.Now;
+                //Xu Ly tu dong Update
+                categories.UpdateAt = DateTime.Now;
+                //Xu Ly tu dong ParentID
+                if (categories.ParentId == null)
+                {
+                    categories.ParentId = 0;
+                }
+                //Xu Ly tu dong Order
+                if (categories.Order == null)
+                {
+                    categories.Order = 1;
+                }
+                else
+                {
+                    categories.Order += 1;
+                }
+                //Xu Ly tu dong Slug
+                categories.Slug = XString.Str_Slug(categories.Name);
+
+                //Chèn thêm dòng cho DB
+                categoriesDAO.Insert(categories);
                return RedirectToAction("Index");
             }
 
@@ -113,6 +138,38 @@ namespace Website_QLy_BanHang.Areas.Admin.Controllers
             categoriesDAO.Delete(categories);
             
             return RedirectToAction("Index");
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        // GET: Admin/Category/Status/5
+        public ActionResult Status(int? id)
+        {
+            if (id == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger","Cập nhập trạng thái thất bại");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Truy van id
+                Categories categories = categoriesDAO.getRow(id);
+
+                // Chuyen Doi Trang Thai cua Status tu 1 <-> 2
+                categories.Status = (categories.Status == 1) ? 2 : 1;
+
+                //Cap nhap gia tri UpdateAt
+                categories.UpdateAt = DateTime.Now;
+
+                //Cap nhap lai DB
+                categoriesDAO.Update(categories);
+
+                //thong bao cap nhap trang thai thanh cong
+                TempData["message"] = new XMessage("success", "Cập nhập trạng thái thành công");
+
+                return RedirectToAction("Index");
+            }
+
+            
         }
     }
 }
